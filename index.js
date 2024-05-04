2ad47ea8882af7b1b0bc3ddfabd63903cb4ab9d1
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-04 16:03:28 +0600

    inserted bulk data to the database and load services data

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const servicesCollection = client.db('carDoctor').collection('services');
+
+
+    app.get('/services', async(req, res) => {
+        const cursor = servicesCollection.find(); 
+        const result = await cursor.toArray();
+        res.send(result);
+      });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
