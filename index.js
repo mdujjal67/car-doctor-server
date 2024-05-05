b72086b7f8e5a238327bbabbb45967804e3116fe
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-05 16:40:27 +0600

    save booking order on the booking collection using post method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -27,6 +27,7 @@ async function run() {
   try {
 
     const servicesCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('booking');
 
 
     app.get('/services', async(req, res) => {
@@ -35,8 +36,42 @@ async function run() {
         res.send(result);
       });
 
+
+      // for specific data
+      app.get('/booking/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)}
+        const option = {
+            projection: {title: 1, price: 1, service_id: 1, img: 1, date: 1}
+        }
+        const result = await servicesCollection.findOne(query, option);
+        res.send(result);
+      })
+
+      // for some data
+      app.get('/booking', async(req, res) => {
+        console.log(req.query.email)
+        let query = {}
+        if(req.query?.email){
+          query = {email: req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray()
+        res.send(result)
+      })
+
+      // for all data
+      app.post('/booking', async(req, res) => {
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result)
+      })
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
