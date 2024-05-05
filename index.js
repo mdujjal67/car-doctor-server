c98e5b9f533b6579631ac5b77f6668c066d1adf8
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-05 18:30:36 +0600

    added deleted method for client server

--- index.js
+++ index.js
@@ -67,6 +67,15 @@ async function run() {
         res.send(result)
       })
 
+      // for delete data
+      app.delete('/booking/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)}
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result);
+      })
+
+
 
 
 
